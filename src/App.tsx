274ae2a4de171488ffac1a9dec2b708a04a78d8c
chris0173 <christopherlacock@gmail.com>
2023-08-29 11:32:43 +1200

    Added routing, some styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
 import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import routes from "./routes";
+import EventForm from "./components/EventForm";
+import EventCard from "./components/EventCard";
+import { EventData } from "./components/EventCard";
 import "./components/styles.css";
+import DarkModeSwitch from "./components/DarkMode";
+import { useCountdownFormat } from "./hooks/useCountdownFormat";
+import logo from "../src/assets/hourglass2_thumbnail.png";
+import { Add } from "@mui/icons-material";
 import {
   Container,
   Grid,
@@ -11,13 +20,6 @@ import {
   ThemeProvider,
   createTheme,
 } from "@mui/material";
-import EventForm from "./components/EventForm";
-import EventCard from "./components/EventCard";
-import { EventData } from "./components/EventCard";
-import { Add } from "@mui/icons-material";
-import DarkModeSwitch from "./components/DarkMode";
-import logo from "../src/assets/hourglass2_thumbnail.png";
-import { useCountdownFormat } from "./hooks/useCountdownFormat";
 
 const darkTheme = createTheme({
   palette: {
@@ -49,47 +51,58 @@ function App() {
   };
 
   return (
-    <ThemeProvider theme={darkTheme}>
-      <AppBar position="sticky">
-        <Toolbar className="NavBar">
-          <img src={logo} width="60px" />
-          <Typography variant="h6" className="title">
-            Event Countdown
-          </Typography>
-          <DarkModeSwitch />
-        </Toolbar>
-      </AppBar>
-      <br />
-      <Container>
-        <Grid container spacing={2}>
-          {events.map((event, index) => (
-            <Grid item xs={12} key={index}>
-              <EventCard
-                selectedFormat={selectedFormat}
-                setSelectedFormat={setSelectedFormat}
-                countdownFormatOptions={countdownFormatOptions}
-                event={event}
-                onDelete={handleDeleteEvent}
-                onEdit={handleEditEvent}
-              />
-            </Grid>
-          ))}
-        </Grid>
-        <Fab
-          color="primary"
-          aria-label="Add Event"
-          onClick={() => setIsFormOpen(true)}
-          style={{ position: "fixed", bottom: "20px", right: "20px" }}
-        >
-          <Add />
-        </Fab>
-        <EventForm
-          open={isFormOpen}
-          onClose={() => setIsFormOpen(false)}
-          onSubmit={handleAddEvent}
-        />
-      </Container>
-    </ThemeProvider>
+    <Router>
+      <ThemeProvider theme={darkTheme}>
+        <AppBar position="sticky">
+          <Toolbar className="NavBar">
+            <img src={logo} width="60px" alt="Logo" />
+            <Typography variant="h6" className="title">
+              Event Countdown
+            </Typography>
+            <div style={{ flexGrow: 1 }} /> {/* Spacer */}
+            <div className="nav-link">
+              <Link to="/register" className="nav-link">
+                Register
+              </Link>
+              <Link to="/login" className="nav-link">
+                Login
+              </Link>
+              <DarkModeSwitch />
+            </div>
+          </Toolbar>
+        </AppBar>
+        <br />
+        <Container>
+          <Grid container spacing={2}>
+            {events.map((event, index) => (
+              <Grid item xs={12} key={index}>
+                <EventCard
+                  selectedFormat={selectedFormat}
+                  setSelectedFormat={setSelectedFormat}
+                  countdownFormatOptions={countdownFormatOptions}
+                  event={event}
+                  onDelete={handleDeleteEvent}
+                  onEdit={handleEditEvent}
+                />
+              </Grid>
+            ))}
+          </Grid>
+          <Fab
+            color="primary"
+            aria-label="Add Event"
+            onClick={() => setIsFormOpen(true)}
+            style={{ position: "fixed", bottom: "20px", right: "20px" }}
+          >
+            <Add />
+          </Fab>
+          <EventForm
+            open={isFormOpen}
+            onClose={() => setIsFormOpen(false)}
+            onSubmit={handleAddEvent}
+          />
+        </Container>
+      </ThemeProvider>
+    </Router>
   );
 }
 
